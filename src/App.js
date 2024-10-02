d9ec2e7f0ab6d33930b7ec03b461d64f358c4fe6
Juanjo <juanjini@gmail.com>
2024-10-02 17:11:24 +0200

    Proyecto Front Basico 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [hora, setHora] = useState(null);
+
+  const obtenerHora = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/hora');
+      const data = await response.json();
+      console.log(data); 
+      setHora(data.hora);
+    } catch (error) {
+      console.error("Error al obtener la hora:", error);
+    }
+  };
+
+  const obtenerHoraDesdeDB = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/db');
+      const data = await response.json();
+      console.log(data);
+      setHora(data[0]?.now);
+    } catch (error) {
+      console.error("Error al obtener la hora desde la base de datos:", error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cliente conectado a la base de datos</h1>
+      <div className="button-container"> {/* Nuevo contenedor */}
+        <button onClick={obtenerHora} className="button">Hora de la petición</button>
+        <button onClick={obtenerHoraDesdeDB} className="button">Hora desde DB</button>
+      </div>
+      {hora && <p>Has hecho click a las: {hora}</p>}
     </div>
   );
 }
